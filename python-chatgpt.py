a68f513abe437ae14991f7bffb08a6ef969f6601
Moksh Agrawal <111683521+Moksh-10@users.noreply.github.com>
2023-06-20 23:40:17 +0530

    Add files via upload

---
+++ python-chatgpt.py
@@ -0,0 +1,23 @@
+import requests
+
+api_endpoint="https://api.openai.com/v1/completions"
+api_key="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"
+
+headers={
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + api_key
+}
+
+request_data={
+    "model":"text-davinci-003",
+    "prompt":"Write python script for hello world",
+    "max_tokens":100,
+    "temperature":0.5
+}
+
+response=requests.post(api_endpoint,headers=headers,json={})
+
+if response.status_code==200:
+    print(response.json())
+else:
+    print(f"Request failed with status code:{str(response.status_code)}")    
